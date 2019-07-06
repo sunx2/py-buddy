9b79057b64c2ed08fb27f1f9edfc7f37d809b111
Sun <xxsunxxrose@gmail.com>
2019-07-06 17:12:08 +0530

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+desc = ""
+version = 1.0
+ldsc = ''
+setup(
+    name= "Python Buddy",
+    version= version,
+    description=desc,
+    long_description=ldsc,
+    author="Sun",
+    author_email="xxsunxxrose@gmail.com",
+    maintainer="Sun",
+    maintainer_email="xxsunxxrose@gmail.com",
+    url="https://sunx2.github.io/py-buddy/",
+    download_url="https://github.com/sunx2/py-buddy/",
+    packages=['py-buddy',],
+    py_modules=[],
+
+)
